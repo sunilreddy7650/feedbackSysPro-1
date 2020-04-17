f72dc43ed2ef9ebcefbbca8ad65d2f5d35898f41
dasrinivas98 <dasrinivas98@gmail.com>
2020-04-17 13:14:46 +0530

    DONE:sending data to mongo anf receiving data from mongo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
+import {FormGroup,FormControl,Validators,FormsModule, } from '@angular/forms';  
+import {CommonService} from './common.service';  
+   
+import {Http,Response, Headers, RequestOptions } from '@angular/http';   
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css']  
+})  
 export class AppComponent {
-  title = 'feedbackSysPro';
+ 
+
+    
+     
+  constructor(private newService :CommonService,public dataservice:CommonService) { 
+
+      this.dataservice.GetUser().subscribe(data =>{this.us = data;
+      console.log(data);
+      });
+      
+
+    }  
+   Repdata; 
+   valbutton ="Save"; 
+   us; 
+   chrtdata: Object[];
+   piedata: any[];
+   
+   show: boolean = false;
+                          
+   public pieChartLabels = ['p-1','p-2','p-3','p-4','p-5','p-6','p-7','p-8','p-9','p-10',];
+  
+  //  public pieChartData = [120, 150, 180, 90,50,76];
+  public pieChartData = [100,200,300,4000,5000];
+   public pieChartType = 'pie';
+   
+    
+
+
+
+   
+   
+ngOnInit() {    
+  this.newService.GetUser().subscribe(data => { 
+    this.Repdata = data;  
+     
+  }) 
+   this.getdata();
+}  
+getdata()
+{
+  
+}
+
+
+
+onSave = function(user,isValid: boolean) {    
+  
+  user.mode= this.valbutton;  
+  this.newService.saveUser(user)  
+  .subscribe(data =>  {  alert(data.data);  
+       
+    this.ngOnInit();    
+  }   
+  , error => this.errorMessage = error )  
+    
+}      
+edit = function(kk) {  
+  
+this.id = kk._id;
+this.usn = kk.usn;  
+this.points= kk.points;  
+this.description= kk.description;  
+this.valbutton ="Update";  
+}  
+  
+delete = function(id) {  
+this.newService.deleteUser(id)  
+.subscribe(data =>   { alert(data.data) ; this.ngOnInit();}, error => this.errorMessage = error )   
+}  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';   
+  
+import { HttpModule } from '@angular/http';  
+import { FormsModule } from '@angular/forms';  
+  
+import { AppComponent } from './app.component';  
+  
+import {CommonService} from './common.service';  
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  
+  
+@NgModule({  
+  declarations: [  
+    AppComponent  
+  ],  
+  imports: [  
+    BrowserModule,HttpModule,FormsModule,  HttpClientModule, ChartsModule, 
+  ],  
+  providers: [CommonService],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  
